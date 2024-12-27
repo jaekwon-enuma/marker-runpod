6b590d69a258e26a7c4db04139c5fb2ba97a4b6d
jaypark <jaypark@gmail.com>
2024-12-27 21:48:41 +0900

    for runpod serverless

--- marker_server.py
+++ marker_server.py
@@ -20,20 +20,27 @@ def handler(event):
       }
     """
 
+    print("handler 01", flush=True)
     input_data = event.get("input", {})
+    print("handler 02", flush=True)
 
     pdf_base64 = input_data.get("pdf_base64")
     if not pdf_base64:
         return {"error": "No 'pdf_base64' found in input."}
 
+    print("handler 03", flush=True)
     fd, temp_filepath = tempfile.mkstemp(suffix=".pdf")
 
     try:
+        print("handler 04", flush=True)
         with open(temp_filepath, "wb") as f:  # Use the file path, not the file descriptor
             f.write(pdf_bytes)
 
+        print("handler 05", flush=True)
         rendered = converter(temp_filepath)
+        print("handler 06", flush=True)
         text, metadata, images = text_from_rendered(rendered)
+        print(f"handler 07 len:{len(text)}", flush=True)
 
         return {
             "markdown": text,
@@ -48,5 +55,3 @@ def handler(event):
 
 if __name__ == "__main__":
     runpod.serverless.start({"handler": handler})
-
-
